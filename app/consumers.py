564feec6c2a57d0d0a98661b4b2c582c55893e7d
Josh Harkema <josh@joshharkema.com>
2018-02-20 14:42:40 -0700

    Code analysis and cleaning.

--- app/consumers.py
+++ app/consumers.py
@@ -36,9 +36,6 @@ class ClassConsumer(AsyncJsonWebsocketConsumer):
         if command == "acknowledge":
             await self.acknowledge(class_id, user_name)  # Uses ID actually.
 
-        if command == "end-clas":
-            await self.end_class(class_id)
-
         if command == "join":
             await self.join_class(class_id, user_name)
 
