d8a175c0cfc866a283dbd7858b9798c9861d46f0
Josh Harkema <josh@joshharkema.com>
2018-02-21 10:09:56 -0700

    Tinkering with mailer.

--- app/functions.py
+++ app/functions.py
@@ -1,6 +1,5 @@
 import smtplib
-from email.header import Header
-from email.mime.text import MIMEText
+import datetime
 
 
 def send_email_report(email, report, class_number):
@@ -14,13 +13,20 @@ def send_email_report(email, report, class_number):
     """
     report = ''.join(report)
 
-    msg = MIMEText(report, "html", "utf-8")
-
-    msg['Subject'] = Header("Hand Tracker Report" + class_number, "utf-8")
-    email_from = "noreply@hand-tracker.com"
-
     s = smtplib.SMTP('mail.joshharkema.com', 587)
-    s.ehlo()
+    s.connect('mail.joshharkema.com', 587)
     s.login('pythonmailer', 'ToyCar11')
-    s.sendmail(email_from, email, msg)
-    s.close()
+
+    from_addr = 'Josh Harkema <josh@joshharkema.com>'
+    to_addr = str(email)
+
+    subj = "Report for Class Number: %s" % class_number
+    date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
+
+    message_text = report
+
+    msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % (
+        from_addr, to_addr, subj, date, message_text)
+
+    s.sendmail(from_addr, to_addr, msg)
+    s.quit()
